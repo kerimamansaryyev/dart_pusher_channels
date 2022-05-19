f5a0f0285eadea33b5e3b6072f4c41b2f7fcdccd
Kerim Amansaryyev <keamansaryyev@gmail.com>
2022-05-19 18:35:11 +0500

    Removed useless fields from PusherException

--- lib/src/exceptions/exception.dart
+++ lib/src/exceptions/exception.dart
@@ -1,9 +1,4 @@
 import 'package:meta/meta.dart';
 
 @immutable
-class PusherException implements Exception {
-  final dynamic error;
-  final StackTrace? stackTrace;
-
-  PusherException({this.error, this.stackTrace});
-}
+class PusherException implements Exception {}
