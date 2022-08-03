83a747995796d1728d7176c3ef823a693e2a13a0
Kerim Amansaryyev <73200585+mcfugger@users.noreply.github.com>
2022-08-03 17:21:41 +0500

    Api library (exports) need to expose almost all structures

--- lib/src/dart_pusher_channels_api.dart
+++ lib/src/dart_pusher_channels_api.dart
@@ -1,6 +1,5 @@
 /// Libraries for creating custom connection implementations
-export 'package:dart_pusher_channels/configs.dart'
-    show PusherChannelsPackageConfigs;
+export 'package:dart_pusher_channels/configs.dart';
 export 'package:dart_pusher_channels/src/channel/channel.dart'
     hide TokenAuthorizationDelegate, PusherTokenAuthDelegateException;
 export 'package:dart_pusher_channels/src/connection.dart';
