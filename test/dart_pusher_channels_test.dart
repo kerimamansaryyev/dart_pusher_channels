08af11c0db12f74d1abe188967a40034b29506f1
Kerim Amansaryyev <keamansaryyev@gmail.com>
2022-08-13 16:26:13 +0500

    Updated tests with the ones on PusherChannelOptions

--- test/dart_pusher_channels_test.dart
+++ test/dart_pusher_channels_test.dart
@@ -1,3 +1,4 @@
+import 'package:dart_pusher_channels/base.dart';
 import 'package:dart_pusher_channels/configs.dart';
 import 'package:test/test.dart';
 
@@ -37,6 +38,114 @@ void main() {
         expect(printedLogs.length, 1);
         expect(printedLogs[0], 'hello');
       });
+    });
+  });
+
+  group('Pusher Channel options uri must be convinient', () {
+    test('Raw constructor', () {
+      const withoutCluster = PusherChannelOptions(
+          scheme: 'ws',
+          host: 'example.com',
+          port: 12,
+          key: 'API_KEY',
+          protocol: 7,
+          version: '7.0.3');
+      expect(
+          withoutCluster.uri,
+          Uri.parse(
+              'ws://example.com:12/app/API_KEY?client=dart&version=7.0.3&protocol=7'));
+      const withCluster = PusherChannelOptions(
+          scheme: 'ws',
+          host: 'example.com',
+          port: 12,
+          cluster: 'CLUSTER',
+          key: 'API_KEY',
+          protocol: 7,
+          version: '7.0.3');
+      expect(
+          withCluster.uri,
+          Uri.parse(
+              'ws://ws-CLUSTER.example.com:12/app/API_KEY?client=dart&version=7.0.3&protocol=7'));
+      const withPath = PusherChannelOptions(
+          scheme: 'ws',
+          host: 'example.com',
+          path: '/CUSTOM_PATH',
+          port: 12,
+          key: 'API_KEY',
+          protocol: 7,
+          version: '7.0.3');
+      expect(
+          withPath.uri,
+          Uri.parse(
+              'ws://example.com:12/CUSTOM_PATH?client=dart&version=7.0.3&protocol=7'));
+    });
+    test('.ws constructor', () {
+      const withoutCluster = PusherChannelOptions.ws(
+          host: 'example.com',
+          port: 12,
+          key: 'API_KEY',
+          protocol: 7,
+          version: '7.0.3');
+      expect(
+          withoutCluster.uri,
+          Uri.parse(
+              'ws://example.com:12/app/API_KEY?client=dart&version=7.0.3&protocol=7'));
+      const withCluster = PusherChannelOptions.ws(
+          host: 'example.com',
+          port: 12,
+          cluster: 'CLUSTER',
+          key: 'API_KEY',
+          protocol: 7,
+          version: '7.0.3');
+      expect(
+          withCluster.uri,
+          Uri.parse(
+              'ws://ws-CLUSTER.example.com:12/app/API_KEY?client=dart&version=7.0.3&protocol=7'));
+      const withPath = PusherChannelOptions.ws(
+          host: 'example.com',
+          path: '/CUSTOM_PATH',
+          port: 12,
+          key: 'API_KEY',
+          protocol: 7,
+          version: '7.0.3');
+      expect(
+          withPath.uri,
+          Uri.parse(
+              'ws://example.com:12/CUSTOM_PATH?client=dart&version=7.0.3&protocol=7'));
+    });
+    test('.wss constructor', () {
+      const withoutCluster = PusherChannelOptions.wss(
+          host: 'example.com',
+          port: 443,
+          key: 'API_KEY',
+          protocol: 7,
+          version: '7.0.3');
+      expect(
+          withoutCluster.uri,
+          Uri.parse(
+              'wss://example.com:443/app/API_KEY?client=dart&version=7.0.3&protocol=7'));
+      const withCluster = PusherChannelOptions.wss(
+          host: 'example.com',
+          port: 443,
+          cluster: 'CLUSTER',
+          key: 'API_KEY',
+          protocol: 7,
+          version: '7.0.3');
+      expect(
+          withCluster.uri,
+          Uri.parse(
+              'wss://ws-CLUSTER.example.com:443/app/API_KEY?client=dart&version=7.0.3&protocol=7'));
+      const withPath = PusherChannelOptions.wss(
+          host: 'example.com',
+          path: '/CUSTOM_PATH',
+          port: 443,
+          key: 'API_KEY',
+          protocol: 7,
+          version: '7.0.3');
+      expect(
+          withPath.uri,
+          Uri.parse(
+              'wss://example.com:443/CUSTOM_PATH?client=dart&version=7.0.3&protocol=7'));
     });
   });
 }
