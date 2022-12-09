c4361145ca9b7bb603d80a32e07792c28ac3a685
Kerim Amansaryyev <keamansaryyev@gmail.com>
2022-12-09 13:53:16 +0500

    Cleaned the project

--- lib/dart_pusher_channels.dart
+++ lib/dart_pusher_channels.dart
@@ -1,6 +1,3 @@
 library dart_pusher_channels;
 
-export 'base.dart';
-export 'api.dart';
-
 const kDartPusherChannelsLibraryVersion = '0.3.1';
--- lib/src/exceptions/exception.dart
+++
@@ -1,7 +0,0 @@
-import 'package:meta/meta.dart';
-
-/// Package's exception type.
-@immutable
-class PusherException implements Exception {
-  const PusherException();
-}
